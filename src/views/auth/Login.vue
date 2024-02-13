f28a41cfb8243e62c904ad74710e9759f9ff9d9e
annamaria <szumiecam@gmail.com>
2024-02-13 17:39:31 +0100

    added Login view

---
+++ src/views/auth/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="email" placeholder="Email" v-model="email" />
+    <input type="password" placeholder="Password" v-model="password" />
+    <div v-if="error" class="error">{{ error }}</div>
+    <button>Log in</button>
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+import useLogin from "@/composables/useLogin";
+
+export default {
+  setup() {
+    const { error, login } = useLogin();
+
+    const email = ref("");
+    const password = ref("");
+
+    const handleSubmit = async () => {
+      const res = await login(email.value, password.value);
+      if (!error.value) {
+        console.log("user logged in ");
+      }
+    };
+
+    return { email, password, handleSubmit, error };
+  },
+};
+</script>
+
+<style>
+</style>
